7784bb68a721c7625b3e73a82af44e9139221045
Lesa McPeak <lesa.mcpeak@wsu.edu>
2018-09-06 14:08:47 -0700

    Remove settings page

--- modules/publications/publications-module.php
+++ modules/publications/publications-module.php
@@ -20,13 +20,6 @@ class Publications_Module extends Core_Module {
 	public $register_args = array(
 		'label'          => 'Publications', // Edit This | Shows on activate module panel
 		'helper_text'    => 'Index of publications.', // Edit This | Shows on activate module panel
-		'settings_page'  => array(
-			'page_title'     => 'Publications Module',
-			'menu_title'     => 'Publications',
-			'capabilities'   => 'manage_options', // Don't touch | Role that can see this
-			'page_slug'      => 'page_slug_here', // Edit This | similar to ID
-			'callback'       => 'render_options_page', // Don't touch
-		),
 	);
 
 	protected $save_args = array(
@@ -366,45 +359,6 @@ class Publications_Module extends Core_Module {
 		register_taxonomy( 'keywords', array( 'publications' ), $keyword_args );
 
 	} // end create_pubs_taxonomies
-
-	// --------------- //
-
-	public function add_admin_settings() {
-
-		$settings_adapter = get_settings_api_adapter(); // Don't touch | Custom settings wrapper to make using it easier
-
-		$page_slug = $this->get_settings_page_slug(); // Don't touch | Gets the page slug for this setting
-
-		$section = 'page_section'; // Edit This | Define your section here
-
-		// Register settings
-
-		$settings_adapter->register_settings( // Don't touch | Registers all of your settings from $this->settings
-			$page_slug,
-			$this->settings
-		);
-
-		$settings_adapter->add_section( // Edit This | Add a custom section
-			$section,
-			'My Section Name Here',
-			$page_slug,
-			'Some random text here' // Edit This | Descriptor text for the section
-		);
-
-		$settings_adapter->add_select_field( // Edit This | Add a select field
-			'setting_key_id',
-			'Setting Label Here',
-			$page_slug, // Don't touch
-			$section, // Don't touch
-			array( // Edit This | Select options as an array
-				'value' => 'Value Label',
-			),
-			get_option( 'setting_key_id' ) // Edit This | Current value of 'setting_key_id'
-		);
-
-	} // End add_settings
-
-
 
 } // End Sub_Layouts
 
